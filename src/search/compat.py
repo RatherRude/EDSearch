52f68372d0161e3e556d6bc7b7e416105d0b0993
Luca Haneklau <git@luca.lsys.ac>
2025-11-13 01:21:32 +0100

    fix: extend example query

--- src/search/compat.py
+++ src/search/compat.py
@@ -1,13 +1,29 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 from Database import pg_connection
 
 router = APIRouter()
 
-@router.get("/stations")
-async def get_stations():
+class StationSearchRequest(BaseModel):
+    coords: tuple[float, float, float]
+
+class StationSearchResponse(BaseModel):
+    stations: list[str]
+
+@router.post("/stations")
+async def post_stations(request: StationSearchRequest) -> StationSearchResponse:
     with pg_connection() as (conn, cur):
         # Start a transaction and set a modest lock timeout
-        cur.execute("SELECT * FROM stations LIMIT 10;")
+        cur.execute("""
+            SELECT 
+                stations.name as station_name, 
+                systems.name AS system_name,
+                systems.coords <-> %s AS distance
+            FROM stations 
+            LEFT JOIN systems on stations.system_id = systems.id
+            ORDER BY systems.coords <-> %s ASC
+            LIMIT 5
+        """, (request.coords, request.coords))
         rows = cur.fetchall()
-        return {"stations": rows}
+        return {"stations": [row.get('name') for row in rows]}
